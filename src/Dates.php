e64eaeb6488655e9105f3d19f9fe832aad2bf4df
Armandexi <armand.tg4@gmail.com>
2024-04-02 15:02:32 +0200

    phpcs

---
+++ src/Dates.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Dates and Setup
+ * 
+ * PHP version 6
+ * 
+ * @category Class
+ * @package  Src
+ * @author   ArmandVan <exemple@mail.com>
+ * @license  http://localhost canard
+ * @link     localhost
+ * This doc determine what Dates do
+ */
+namespace UPJV;
+
+/**
+ * Dates and Setup
+ * 
+ * @category Class
+ * @package  Src
+ * @author   ArmandVan <exemple@mail.com>
+ * @license  http://localhost canard
+ * @link     localhost
+ * @return   void
+ * This doc determine what Dates do
+ */
+
+class Dates
+{
+    /**
+     * Donne date
+     * 
+     * @return bool
+     */
+    public function demain()
+    {
+        $demain = new \DateTime();
+        $demain->add(new \DateInterval(duration:'P10'));
+        $reponse = ['demain' => $demain->format(format:"d-m-Y")];
+        return json_encode($reponse);
+    }
+}
